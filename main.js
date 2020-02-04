eae911339e1afd1360be5eae780a270c593d79d7
Brian Bonsignore <bonsignore.brian@gmail.com>
2020-02-04 14:02:36 -0500

    Adds functions to capitalize and get customer names

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+// ===========  Build the page ======================
+
+// Returns the element and h1 tag for the customer database to be displayed in
+function createMain() {
+    let container = createElement('div', ['flex', 'main'])
+    let h1 = document.createElement('h1', ['h1'])
+    h1.textContent = "Customer Database"
+    container.appendChild(h1)
+    return container
+}
+
+
+
+// ========== Helper functions ============================
+
 // type should be a type of html element, as a string
 // classArr should be an array of strings to be used as HTML classes
 function createElement(type, classArr) {
@@ -6,20 +21,38 @@ function createElement(type, classArr) {
     return element
 }
 
-// Returnas the element and h1 tag for the customer database to be displayed in
-function createMain() {
-    let container = createElement('div', ['flex', 'main'])
-    let h1 = document.createElement('h1',['h1'])
-    h1.textContent = "Customer Database"
-    container.appendChild(h1)
-    return container
+// returns a string with the first character capitalized
+function capitalize(string) {
+    if (string.length === 1) {
+        return string[0].toUpperCase()
+    }
+    else {
+        return string[0].toUpperCase() + string.slice(1)
+    }
 }
 
 
 
-function buildPage() {
-    const body = document.querySelector('body')
-    let container = createMain()
-    body.appendChild(container)
+
+//  =================== Functions using customer objects ========================
+// customer should be an object from the array customers" in customers.js
+function customerName(customer) {
+    let components = [customer['name'].title + '.', customer['name'].first, customer['name'].last]
+    for (let i = 0; i < components.length; i++) {
+        components[i] = capitalize(components[i])
+    }
+    return components.join(' ')
+}
+
+function customerLocation (customer) {
 
 }
+
+// function createCard(customer) {
+//     const card = createElement('div', ['card', 'flex'])
+//     const img = createElement('img', ['face'])
+//     img.src = customer['picture']
+//     img.alt = `Profile picture of ${customer.}`
+// }
+
+
